aa6ea700c1b257ba1f981e311a59cb8c6e8fb688
hfg-mikias-e <mikias.ehrmann@hfg.design>
2024-04-11 22:43:33 +0200

    Update Play.vue

--- src/components/Play.vue
+++ src/components/Play.vue
@@ -219,6 +219,8 @@
         const hands = handouts[0]
         const firstTurn = handouts[1]
 
+        alert(handouts)
+
         this.players.forEach((player: Player, index: number) => {
           player.hand = hands[index]
           player.turn = index === firstTurn
